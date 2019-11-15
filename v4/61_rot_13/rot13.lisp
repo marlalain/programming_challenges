c897061b7caecab89fe228f3c767448e605829be
Paulo Elienay <elie9alb@gmail.com>
2019-11-15 12:22:19 -0400

    #61 done but needs one fix

--- v4/61_rot_13/rot13.lisp
+++ v4/61_rot_13/rot13.lisp
@@ -1,10 +1,21 @@
-; LISP
+; ROT13 (LISP)
+; Paulo Elienay II
+;
+; ROT13 ("rotate by 13 places", sometimes hyphenated ROT-13) is a simple letter substitution cipher
+; that replaces a letter with the 13th letter after it, in the alphabet. ROT13 is a special case of
+; the Caesar cipher which was developed in ancient Rome. (https://en.wikipedia.org/wiki/ROT13)
 
-(print "Hello World!")
-(format t "Hello, again ~%")
+(setq *print-case* :upcase)
 
-(print "What's your name?")
-(defvar *name* (read))
-(format t "Hi, ~a ~%" *name*)
-
-(setq *print-case* :capitalize-first)
+(defun get_message ()
+  (format t "What's your message? ~%")
+  (defvar message (read-line))
+  (defvar message_list (coerce message 'list))
+  ;; (> 77) (< 78)
+  (loop for letter in message_list
+        do (if (< (char-code letter) 110)
+               (format t "~a" (code-char (+ (char-code letter) 13)))
+               (format t "~a" (code-char (- (char-code letter) 13))))
+    )
+  )
+(get_message)
