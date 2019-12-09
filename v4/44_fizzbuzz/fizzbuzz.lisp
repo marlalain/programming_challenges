fa3c6f19729676a39897a4e85ad01efc80f292ae
Paulo Elienay <elie9alb@gmail.com>
2019-12-09 12:22:50 -0300

    Finished #44 in CLisp

---
+++ v4/44_fizzbuzz/fizzbuzz.lisp
@@ -0,0 +1,11 @@
+;;; FizzBuzz in Common Lisp
+;;; Made By Paulo Elienay II
+
+(defun fizzbuzz (range)
+  (loop for i from 0 to range do (cond
+                                   ((= (mod i 15) 0) (print (concatenate 'string (write-to-string i) ": FizzBuzz")))
+                                   ((= (mod i 3) 0) (print (concatenate 'string (write-to-string i) ": Fizz")))
+                                   ((= (mod i 5) 0) (print (concatenate 'string (write-to-string i) ": Buzz")))
+                                   )))
+
+(fizzbuzz 100)
