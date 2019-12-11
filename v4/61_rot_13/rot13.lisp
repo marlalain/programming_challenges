c9a40bf7f8f4f1b831f74835eef60fbbe8e22986
Paulo Elienay <elie9alb@gmail.com>
2019-12-10 21:02:58 -0300

    Finished #61

--- v4/61_rot_13/rot13.lisp
+++ v4/61_rot_13/rot13.lisp
@@ -1,9 +1,9 @@
-; ROT13 (LISP)
-; Paulo Elienay II
-;
-; ROT13 ("rotate by 13 places", sometimes hyphenated ROT-13) is a simple letter substitution cipher
-; that replaces a letter with the 13th letter after it, in the alphabet. ROT13 is a special case of
-; the Caesar cipher which was developed in ancient Rome. (https://en.wikipedia.org/wiki/ROT13)
+;;; ROT13 (LISP)
+;; Paulo Elienay II
+
+;; ROT13 ("rotate by 13 places", sometimes hyphenated ROT-13) is a simple letter substitution cipher
+;; that replaces a letter with the 13th letter after it, in the alphabet. ROT13 is a special case of
+;; the Caesar cipher which was developed in ancient Rome. (https://en.wikipedia.org/wiki/ROT13)
 
 (setq *print-case* :upcase)
 
@@ -13,9 +13,14 @@
   (defvar message_list (coerce message 'list))
   ;; TODO ADD IFELSE FOR: (> 77) (< 78)
   (loop for letter in message_list
-        do (if (< (char-code letter) 110)
-               (format t "~a" (code-char (+ (char-code letter) 13)))
-               (format t "~a" (code-char (- (char-code letter) 13))))
-    )
-  )
+        do (cond
+             ((< (char-code letter) 110) (format t "~a" (code-char (+ (char-code letter) 13))))
+             ((> (char-code letter) 100) (format t "~a" (code-char (- (char-code letter) 13))))
+             ;;((< (char-code letter) 78) (format t "~a" (code-char (+ (char-code letter) 13))))
+             ;;((> (char-code letter) 77) (format t "~a" (code-char (- (char-code letter) 13))))
+             )
+    ;;     do (if (< (char-code letter) 110)
+    ;;            (format t "~a" (code-char (+ (char-code letter) 13)))
+    ;;            (format t "~a" (code-char (- (char-code letter) 13))))
+  ))
 (get_message)
