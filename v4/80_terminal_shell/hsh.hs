31e07056bf6a8ea04fe252d60ef0b1cb2f86acdf
Paulo Elienay <elie9alb@gmail.com>
2019-12-11 22:03:28 -0300

    Changing #80 from Haskell to C

--- v4/80_terminal_shell/hsh.hs
+++
@@ -1,8 +0,0 @@
--- hsh - The Haskell Shell Terminal
--- Made by Paulo Elienay II
-
--- imports
-import System.Environment
-
--- testing imports
-main = putStrLn "USER: " >> (print =<< getEnv "USER")
