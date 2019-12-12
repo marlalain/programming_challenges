e74eee7e40c7887ca0e4f218a2b8eeb8698b7ef3
Paulo Elienay <elie9alb@gmail.com>
2019-12-11 22:04:09 -0300

    TODO Fix this

--- v4/44_fizzbuzz/fizzbuzz.lisp
+++ v4/44_fizzbuzz/fizzbuzz.lisp
@@ -5,7 +5,6 @@
   (loop for i from 0 to range do (cond
                                    ((= (mod i 15) 0) (print (concatenate 'string (write-to-string i) ": FizzBuzz")))
                                    ((= (mod i 3) 0) (print (concatenate 'string (write-to-string i) ": Fizz")))
-                                   ((= (mod i 5) 0) (print (concatenate 'string (write-to-string i) ": Buzz")))
-                                   )))
+                                   ((= (mod i 5) 0) (print (concatenate 'string (write-to-string i) ": Buzz"))))))
 
 (fizzbuzz 100)
