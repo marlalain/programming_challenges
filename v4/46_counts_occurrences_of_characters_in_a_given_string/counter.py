60a504908d24ef61570c1c777b5dac6a20766685
Paulo Elienay <elie9alb@gmail.com>
2019-12-17 01:24:23 -0300

    Starting #46

---
+++ v4/46_counts_occurrences_of_characters_in_a_given_string/counter.py
@@ -0,0 +1,19 @@
+#!/env/python3
+# Paulo Elienay II
+
+# imports
+import string
+# vars
+alphabet = string.ascii_lowercase
+alphabet_list = list(string.ascii_lowercase)
+# defs
+def count(string):
+    # code
+# main
+def main():
+    print("This script counts occurrences of characters in a given string")
+    count(input("Enter string: "))
+
+
+if __name__ == '__main__':
+    main()
