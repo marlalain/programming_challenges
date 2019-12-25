b2fa28320f9eadeeb2d078b3ae0719d59cd3438a
Paulo Elienay <elie9alb@gmail.com>
2019-12-25 13:37:04 -0300

    Generating the center of the room #121

--- v4/121_roguelike_engine_dungeon_generator/main.c
+++ v4/121_roguelike_engine_dungeon_generator/main.c
@@ -1,16 +1,24 @@
 #include "termbox.h"
+#include <math.h>
+#include <openssl/rand.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include <unistd.h>
 //#include <wchar.h>
 
 // TODO Add Resize
 
+struct room {
+  int center_x;
+  int center_y;
+} room;
+
 int initialize_map(int w, int h) {
   for (int i = 0; i < w; i++) {
     for (int j = 0; j < h; j++) {
-      tb_change_cell(i, j, '.', TB_WHITE, TB_DEFAULT);
+      tb_change_cell(i, j, ' ', TB_WHITE, TB_DEFAULT);
     }
   }
   return 0;
@@ -19,6 +27,7 @@ int initialize_map(int w, int h) {
 int main() {
   tb_init();
   tb_clear();
+  srand((unsigned)time(NULL));
 
   int w, h;
   w = tb_width();
@@ -26,6 +35,14 @@ int main() {
   struct tb_event env;
 
   initialize_map(w, h);
+
+  // 1. Generate first cell
+  struct room first_room;
+  first_room.center_x = (rand() % w) + 1;
+  first_room.center_y = (rand() % h) + 1;
+  tb_change_cell(first_room.center_x, first_room.center_y, '#', TB_WHITE,
+                 TB_DEFAULT);
+
   tb_present();
   tb_poll_event(&env);
 
